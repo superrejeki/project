8e6e38d6af695ca555dbfca5ca1b4afb600a94c7
MAC <mac@MACs-MacBook-Pro.local>
2020-01-06 09:43:11 +0700

    nambah session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session')
 
 const { Pool } = require('pg')
 const pool = new Pool({
@@ -28,6 +29,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'superrahasia'
+}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
