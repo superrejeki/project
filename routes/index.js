b0bd753fd0c04bfd175883d2f4f8144fe8b0fa34
MAC <mac@MACs-MacBook-Pro.local>
2020-01-06 09:15:58 +0700

    login.html

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,19 @@ var router = express.Router();
 /* GET home page. */
 
 module.exports = function(pool){
-router.get('/', function(req, res, next) {
-  pool.query('SELECT * FROM users', (err, response) => {
-    if(err) return res.send(err)
-    res.render('index', { data: response.rows });
-  })
-});
+
+  /* GET home page. */
+  router.get('/', function(req, res, next) {
+    res.render('login',{
+
+    });
+  });
+
+// router.get('/', function(req, res, next) {
+//   pool.query('SELECT * FROM users', (err, response) => {
+//     if(err) return res.send(err)
+//     res.render('/login', { data: response.rows });
+//   })
+// });
 return router;
 }
