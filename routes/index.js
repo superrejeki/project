dba8357fb287a09b70f9755baec891f0e4872113
MAC <mac@MACs-MacBook-Pro.local>
2020-01-06 10:52:39 +0700

    nambah login

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 
 module.exports = function(pool){
-
   /* GET home page. */
   router.get('/', function(req, res, next) {
     res.render('login',{
-
     });
   });
-
-// router.get('/', function(req, res, next) {
-//   pool.query('SELECT * FROM users', (err, response) => {
-//     if(err) return res.send(err)
-//     res.render('/login', { data: response.rows });
-//   })
-// });
+router.post('/login', function(req, res, next) {
+  const emailuser = req.body.emailuser;
+  const password = req.body.password;
+  if( emailuser && password ){
+    pool.query(`SELECT * FROM users WHERE email = '${emailuser}' AND password = '${password}'`, 
+    (err, response) => {
+      if (response.rows.length > 0){
+        res.redirect('/')
+      }else{
+        res.send('email atau password salah')
+      }
+    })
+  }else{
+    res.send('lengkapi form')
+  }
+})
 return router;
 }
